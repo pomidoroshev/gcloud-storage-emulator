8ea825a46cd6da81c6f9dafda5c28f12d0f07677
Alessandro Artoni <artoale@potatolondon.com>
2020-06-22 18:37:19 +0100

    Allow dots in bucket names

--- gcloud_storage_emulator/server.py
+++ gcloud_storage_emulator/server.py
@@ -33,23 +33,23 @@ def _health_check(req, res, storage):
 
 HANDLERS = (
     (r"^{}/b$".format(settings.API_ENDPOINT), {GET: buckets.ls, POST: buckets.insert}),
-    (r"^{}/b/(?P<bucket_name>[-\w]+)$".format(settings.API_ENDPOINT), {GET: buckets.get, DELETE: buckets.delete}),
+    (r"^{}/b/(?P<bucket_name>[-.\w]+)$".format(settings.API_ENDPOINT), {GET: buckets.get, DELETE: buckets.delete}),
     (
-        r"^{}/b/(?P<bucket_name>[-\w]+)/o$".format(settings.API_ENDPOINT),
+        r"^{}/b/(?P<bucket_name>[-.\w]+)/o$".format(settings.API_ENDPOINT),
         {GET: objects.ls}
     ),
     (
-        r"^{}/b/(?P<bucket_name>[-\w]+)/o/(?P<object_id>[-%.\w]+)$".format(settings.API_ENDPOINT),
+        r"^{}/b/(?P<bucket_name>[-.\w]+)/o/(?P<object_id>[-%.\w]+)$".format(settings.API_ENDPOINT),
         {GET: objects.get, DELETE: objects.delete}
     ),
 
     # Non-default API endpoints
     (
-        r"^{}/b/(?P<bucket_name>[-\w]+)/o$".format(settings.UPLOAD_API_ENDPOINT),
+        r"^{}/b/(?P<bucket_name>[-.\w]+)/o$".format(settings.UPLOAD_API_ENDPOINT),
         {POST: objects.insert, PUT: objects.upload_partial}
     ),
     (
-        r"^{}/b/(?P<bucket_name>[-\w]+)/o/(?P<object_id>[-%.\w]+)$".format(settings.DOWNLOAD_API_ENDPOINT),
+        r"^{}/b/(?P<bucket_name>[-.\w]+)/o/(?P<object_id>[-%.\w]+)$".format(settings.DOWNLOAD_API_ENDPOINT),
         {GET: objects.download},
     ),
 
@@ -58,7 +58,7 @@ HANDLERS = (
     (r"^/wipe$", {GET: _wipe_data}),  # Wipe all data
 
     # Public file serving, same as object.download
-    (r"^/(?P<bucket_name>[-\w]+)/(?P<object_id>[-%.\w]+)$", {GET: objects.download}),
+    (r"^/(?P<bucket_name>[-.\w]+)/(?P<object_id>[-%.\w]+)$", {GET: objects.download}),
 )
 
 
